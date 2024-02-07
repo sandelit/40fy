410174adbc202c17e4837d5015c079d65226b9ab
sandelit <thomas.sandelin@gmail.com>
2024-02-07 18:57:07 +0200

    initial commit

---
+++ src/components/mod.rs
@@ -0,0 +1,4 @@
+pub mod top_bar; 
+pub mod side_bar; 
+pub mod entry_list; 
+pub mod active_entry; 
---
+++ src/utils/tauri_commands.rs
@@ -0,0 +1,9 @@
+
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsValue;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
+    pub async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+}
